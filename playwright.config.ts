f555f835b5329145e2ede936848fd30765e57cb5
zkz098 <zkz20081204@126.com>
2026-04-18 15:04:27 +0800

    chore: README

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   testDir: "./tests/e2e",
   fullyParallel: true,
   forbidOnly: Boolean(process.env.CI),
-  retries: process.env.CI ? 1 : 0,
+  retries: process.env.CI ? 3 : 0,
   reporter: [["list"], ["html", { open: "never" }]],
   timeout: 30_000,
   expect: {
